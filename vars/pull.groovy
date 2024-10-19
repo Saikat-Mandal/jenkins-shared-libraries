e47d0aa663f9a843db22c02b6f076ec8255f9369
Saikat Mandal <75900118+Saikat-Mandal@users.noreply.github.com>
2024-10-19 19:10:40 +0530

    Update pull.groovy

--- vars/pull.groovy
+++ vars/pull.groovy
@@ -1,5 +1,5 @@
-def call(){
+def call(String url , String branch){
   echo 'Pulling the code'
-  git url: "https://github.com/LondheShubham153/django-notes-app" , branch: "main"
+  git url: url , branch: branch
   echo " code pulled successfully !"
 }
